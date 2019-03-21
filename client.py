f8fb447c158b3674346fdcf11e85467f94ef6367
filipe <fcarvalho1@bitbucket.org>
2019-03-21 14:48:02 +0000

    changed to oop

--- client.py
+++ client.py
@@ -1,35 +1,50 @@
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt 
 import time
+import uuid
 
 
 
-
-broker_address="192.168.2.53"
+broker_address="localhost"
 topic = "#"
 
-configTopic = "homeassistant/switch/{}/config" 
-stateTopic = "stat/{}/state" 
-cmdTopic = "cmd/{}/exec" 
+conf = {
+    "configTopic" : "homeassistant/switch/win/config", 
+    "stateTopic" : "stat/win/state" ,
+    "cmdTopic" : "cmd/win/exec",
+    "lwtTopic" : "some/thing/lwt"
+}
 
+stateMsgTemplate ='{{"name":"bedroom_heater","command_topic":"{cmd_topic}","state_topic":"{state_topic}","value_template":"{{value_json.POWER}}","payload_off":"OFF","payload_on":"ON","availability_topic":"{lwt_topic}","payload_available":"Online","payload_not_available":"Offline"}}' 
 
+class WinClient:
+    
 
-#broker_address="iot.eclipse.org"
-print("creating new instance")
-client = mqtt.Client("P1") #create new instance
-client.on_message=on_message #attach function to callback
+    def __init__(self, host, conf):
+        self.client = mqtt.Client(str(uuid.uuid4))
+        print(host)
+        self.client.connect(host)
+        self.confs = [conf]
+        self.subTopics()
+        self.pushConfigs()
+        self.client.on_message = self.on_message
 
-print("connecting to broker")
-client.connect(broker_address) #connect to broker
-
-client.loop_start() #start the loop
-
-print("Subscribing to topic",topic)
-client.subscribe(topic)
-
-time.sleep(1200) # wait
-client.loop_stop() #stop the loop
-
-
-def on_message(client, userdata, message):
+    def on_message(self, client, userdata, message):
         print("message received " ,str(message.payload.decode("utf-8")))
         print("message topic=",message.topic)
+    
+    def subTopics(self):
+        for c in self.confs:
+            cmdTopic = c['cmdTopic']
+            self.client.subscribe(cmdTopic)
+            print("Subscribing to topic",cmdTopic)
+    
+    def pushConfigs(self):
+        for c in self.confs:
+            msg = stateMsgTemplate.format(cmd_topic=c['cmdTopic'], state_topic=c['stateTopic'], lwt_topic=c['lwtTopic'])
+            self.client.publish(c["configTopic"], msg)
+    
+    def run(self):
+        self.client.loop_start()
+
+w = WinClient('127.0.0.1', conf)
+w.run()
