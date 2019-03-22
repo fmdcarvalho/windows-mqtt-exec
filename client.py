fa630b7612294f9ff2fcbabf16b8d881e749a044
filipe <fcarvalho1@bitbucket.org>
2019-03-22 11:33:59 +0000

    i dont like good commit messages

--- client.py
+++ client.py
@@ -2,35 +2,36 @@ import paho.mqtt.client as mqtt
 import time
 import uuid
 
-
-
-broker_address="localhost"
-topic = "#"
-
 conf = {
+    "name" : "win",
     "configTopic" : "homeassistant/switch/win/config", 
     "stateTopic" : "stat/win/state" ,
     "cmdTopic" : "cmd/win/exec",
     "lwtTopic" : "some/thing/lwt"
 }
 
-stateMsgTemplate ='{{"name":"bedroom_heater","command_topic":"{cmd_topic}","state_topic":"{state_topic}","value_template":"{{value_json.POWER}}","payload_off":"OFF","payload_on":"ON","availability_topic":"{lwt_topic}","payload_available":"Online","payload_not_available":"Offline"}}' 
+configMsgTemplate ='{{"name": "{name}","command_topic":"{cmd_topic}","state_topic":"{state_topic}","value_template":"{{value_json.POWER}}","payload_off":"OFF","payload_on":"ON","availability_topic":"{lwt_topic}","payload_available":"Online","payload_not_available":"Offline"}}' 
+
+stateMsgTemplate = '{{"POWER":"{state}"}}'
 
 class WinClient:
     
 
-    def __init__(self, host, conf):
+    def __init__(self, host, conf, port=1883):
         self.client = mqtt.Client(str(uuid.uuid4))
         print(host)
-        self.client.connect(host)
+        self.client.connect(host, port)
         self.confs = [conf]
         self.subTopics()
+        self.pushLWT()
         self.pushConfigs()
+        self.pushStates()
         self.client.on_message = self.on_message
 
     def on_message(self, client, userdata, message):
         print("message received " ,str(message.payload.decode("utf-8")))
         print("message topic=",message.topic)
+        self.pushStates()
     
     def subTopics(self):
         for c in self.confs:
@@ -40,11 +41,24 @@ class WinClient:
     
     def pushConfigs(self):
         for c in self.confs:
-            msg = stateMsgTemplate.format(cmd_topic=c['cmdTopic'], state_topic=c['stateTopic'], lwt_topic=c['lwtTopic'])
+            msg = configMsgTemplate.format(name=c['name'], cmd_topic=c['cmdTopic'], state_topic=c['stateTopic'], lwt_topic=c['lwtTopic'])
+            print(msg)
             self.client.publish(c["configTopic"], msg)
-    
+
+    def pushLWT(self):
+        for c in self.confs:
+            self.client.publish(c["lwtTopic"], "Online", retain=True)
+
+    def pushStates(self):
+        for c in self.confs:
+            msg = stateMsgTemplate.format(state="ON")
+            print(msg)
+            self.client.publish(c["stateTopic"], '{"POWER":"ON"}', retain=True)
+
     def run(self):
         self.client.loop_start()
+        while True:
+            time.sleep(1000)
 
-w = WinClient('127.0.0.1', conf)
+w = WinClient('127.0.0.1', conf, 7001)
 w.run()
